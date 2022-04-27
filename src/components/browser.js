4ad5605063e53025543ee3c9a2195d5db2765324
Mohammd Mustafa <momust187@gmail.com>
2022-04-27 14:05:31 +0200

    Singel object printout working

--- src/components/browser.js
+++ src/components/browser.js
@@ -3,16 +3,19 @@ import '../css/browser.css';
 import '../css/front.css';
 import TripList from './tripList';
 
-
+//This is browser page 
 function Browser() {
-
+  //Use state for changing the state of the page
   const [trip, setTrips] = useState(null)
 
   useEffect(() =>{
-    fetch('http://localhost:8000/trips')
+    //Fetch objects from json database
+    fetch(`http://localhost:8000/trips`)
+    //After fetch wait for response and convert to json
     .then(res =>{
-      return res.json();
+      return res.json(); 
     })
+    //Get data from json resp and set new state with data
     .then(data =>{
       setTrips(data);
     })
@@ -20,10 +23,7 @@ function Browser() {
   return (
     <div id='body'>
       {trip && <TripList trips={trip}/>}
-        
     </div>
-      
-    
   );
 }
 
--- src/components/tripList.js
+++ src/components/tripList.js
@@ -1,8 +1,15 @@
 
 
 function TripList({trips}) {
+    //Check if its not an array if so set prop to a singel element array. (For singel objects)
+    if (!Array.isArray(trips)){
+        trips = [trips];
+    }
+    
 return(
+    <>
      <div  className="trip-list">
+         
          {trips.map((trip)=>(
              <div key={trip.id} className='row head-row p-0 m-0 mt-5'>
                 <div className="card-wrapper col-6">
@@ -24,6 +31,8 @@ return(
           </div>
          ))}
      </div>
+    </>
+    
 )
  
 
