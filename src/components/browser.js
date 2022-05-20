b29d08149f5e895df5c6164148f54c685ecafc01
Mohammd Mustafa <momust187@gmail.com>
2022-05-20 20:23:25 +0200

    search working

--- src/components/browser.js
+++ src/components/browser.js
@@ -1,4 +1,4 @@
-
+import { useNavigate } from "react-router-dom";
 import '../css/browser.css';
 import '../css/front.css';
 import useFetch from './useFetch';
@@ -9,15 +9,48 @@ import CityList from './cityList';
 function Browser() {
  //Run useFetch function, return data of db and is loading state, 
  const {data, isLoading} = useFetch(`http://localhost:8000/dataCities`)
- 
+  let navigate = useNavigate(); 
+  function clickHandler(){
+    let userInput = document.getElementById("search").value;
+    //Checks user input and displays correct spots
+    for (let i = 0; i < data.length; i++) {
+                let element = data[i];
+                //If input matches 
+                if(element.name == userInput){
+                   localStorage.setItem('objId', element.cityId);
+                  let path = `../citySpots`;
+                  
+                    navigate(path);
+                    location.reload(); 
+                    
+                    console.log(element)
+                    console.log(element.cityId)
+                   
+                }else{
+                  console.log('Cannot find what you are looking')
+                  document.querySelector('#fail').innerText = 'Cannot find what you are looking';
+                }
+            }
+  // console.log(userInput)
+  }
+  
+  // console.log('Check data')
+  // console.log(data)
   return (
     <>
     {/* Call nav componenet */}
     <Nav />
+    <input 
+    placeholder="Search"
+    type="text"
+    id="search" 
+    >
+    </input>
+    <button onClick={clickHandler}>Search</button>
     {/* If data fetched is not showing, show is loading message state */}
      {isLoading &&
       <div className='loading-wrapper'>
-         <div className="loading">Loading...</div>
+         <div id="fail" className="loading">Loading...</div>
       </div>}
       {/* Get data and pass to CityList component */}
     <div id='body'>{data && <CityList cities={data}/>}
--- src/components/cityList.js
+++ src/components/cityList.js
@@ -10,19 +10,30 @@ let cityKey = '';
 //Function/Component Get citites
 function CityList({cities}) {
     //Check if its not an array if so set prop to a singel element array. (For singel objects)
-    if (!Array.isArray(cities)){
-        cities = [cities];
-    }
+    // if (!Array.isArray(cities)){
+    //     cities = [cities];
+    // }
     let data = cities;
     let output = '';
     // console.log("cities")
-    // console.log(cities);
-    
-return(
-    <>
-    {/* Run foreach for every object from the data passed to CityList */}
-         {data.forEach(city => {
-             output +=`
+    // console.log(data);
+    /* Run foreach for every object from the data passed to CityList */
+
+    // for (let i = 0; i < data.length; i++) {
+    //             let element = data[i];
+    //             if(element.name == "Göteborg"){
+    //                 data = element
+    //             }
+    //         }
+
+            // console.log(data.name)
+        if(Array.isArray(data)){
+            
+
+
+            data.forEach(city => {
+                
+                    output +=`
        <a href="./citySpots"><div class="card" onclick="getInfo(${city.cityId})">
             <div class="card-image" style="background: url(${city.image})"></div>
           <div class="card-text">
@@ -32,9 +43,36 @@ return(
           </div>
           <div class="card-stats"></div>
           </div></a>`;
-            //  Output all objects to body div in browser componentpåpå
-          document.getElementById('body').innerHTML = output;
-         })}
+            
+          //  Output all objects to body div in browser componentpåpå
+            document.getElementById('body').innerHTML = output
+         
+                })
+             
+        }else{
+            let name = data.name
+            let description = data.description
+            let cityId = data.cityId
+
+            output += `<a href="./citySpots"><div class="card" onclick="getInfo(${cityId})">
+            
+          <div class="card-text">
+              <span class="date">4 days</span>
+              <h2>${name}</h2>
+              <p>${description}</p>
+          </div>
+          <div class="card-stats"></div>
+          </div></a>`;
+
+          document.getElementById('body').innerHTML = output
+        }
+        
+    
+    
+    
+return(
+    <>
+         
     </>
     
 )
