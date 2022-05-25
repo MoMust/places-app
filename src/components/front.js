5483f509de299bd899a5658508e13bcb59d59c96
Mohammd Mustafa <momust187@gmail.com>
2022-05-25 19:09:24 +0200

    Landing page set

--- src/components/front.js
+++ src/components/front.js
@@ -4,24 +4,31 @@ import Nav from './nav'
 function Front() {
   return (
     <>
-      <div className="background">
-       <Nav/>
-        <div className="content">
-          <div className="header-title">
-            <h1>Trip planning made easy</h1>
-          </div>
-          <div className="card-row">
-            <div className="header-card col-8 row">
-              <div className="header-card-text col-12">
-                <p>
-                  View Swedens most attractive places. Dont miss out on exploring the biggest citites like Stockholm, Malmö and Göteborg. Create your own place and save it, make it easy.
-                </p>
+      
+        <div className="background">
+          <Nav />
+          <div className="content">
+            <div className="header-title">
+              <h1>Trip planning made easy</h1>
+            </div>
+            <div className="card-row">
+              <div className="header-card col-8 row">
+                <div className="header-card-text col-12">
+                  <p>
+                    View Swedens most attractive places. Dont miss out on
+                    exploring the biggest citites like Stockholm, Malmö and
+                    Göteborg. Create your own place and save it, make it easy.
+                  </p>
+                </div>
               </div>
             </div>
-            
+            <div className="btn-front">
+              <button>Find cities</button>
+              <button>Click</button>
+            </div>
           </div>
         </div>
-      </div>
+    
     </>
   );
 }
